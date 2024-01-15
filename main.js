610e8c0ae0d4c29dd9e416cf4a3fa0cedea0025b
Aarush2008 <98787426+Aarush2008@users.noreply.github.com>
2024-01-15 09:03:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Simple Addition Calculator
+
+// Btn Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  // Input
+  let num1 = document.getElementById("num1-in").value;
+  let num2 = document.getElementById("");
+  // Process
+  
+  // Output
+}
